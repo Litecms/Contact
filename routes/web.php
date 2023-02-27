58f4dd0113b55ec7aa5230e6cac21ba9477172b2
George John <george@renfos.com>
2023-02-27 12:26:43 +0400

    Update for Lavalite 10

--- routes/web.php
+++ routes/web.php
@@ -15,25 +15,25 @@ Route::get('contact/{slug?}', 'ContactPublicController@show');
 Route::post('contact/sendmail', 'ContactPublicController@sendMail');
 
 
-if (Trans::isMultilingual()) {
-    Route::group(
-        [
-            'prefix' => '{trans}',
-            'where'  => ['trans' => Trans::keys('|')],
-        ],
-        function () {
-            // Guard routes for contact
-            Route::prefix('{guard}/contact')->group(function () {
-                Route::resource('contact', 'ContactResourceController');
-            });
+// if (Trans::isMultilingual()) {
+//     Route::group(
+//         [
+//             'prefix' => '{trans}',
+//             'where'  => ['trans' => Trans::keys('|')],
+//         ],
+//         function () {
+//             // Guard routes for contact
+//             Route::prefix('{guard}/contact')->group(function () {
+//                 Route::resource('contact', 'ContactResourceController');
+//             });
             
             
 
-            // Public routes for contact
-            Route::get('contact.htm', 'ContactPublicController@index');
-            Route::get('contact/{slug?}', 'ContactPublicController@show');
-            Route::post('contact/sendmail', 'ContactPublicController@sendMail');
+//             // Public routes for contact
+//             Route::get('contact.htm', 'ContactPublicController@index');
+//             Route::get('contact/{slug?}', 'ContactPublicController@show');
+//             Route::post('contact/sendmail', 'ContactPublicController@sendMail');
             
-        }
-    );
-}
+//         }
+//     );
+// }
